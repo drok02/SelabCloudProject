acaa01a733274c22f9e6bdefc4f2621f261de99f
이봉훈 <drok02@naver.com>
2022-02-23 14:45:38 +0900

    bbb

--- main.py
+++ main.py
@@ -44,28 +44,28 @@ class AccountView():
 
     def create_stack():
         admin_token= AccountView.token()
-        system_num=int(input("원하는 시스템 번호를 입력: 1.Ubuntu 2.CentOS 3.Fedora\n"))
-        stack_name= input("stack 이름 입력 : ")
-        key_name= input("key 이름 입력 : ")
-        server_name= input("server 이름 입력 : ") 
-        num_user=int(input("사용자 수 입력: ")) 
-        with open('main.json','r') as f:
+        # system_num=int(input("원하는 시스템 번호를 입력: 1.Ubuntu 2.CentOS 3.Fedora\n"))
+        # stack_name= input("stack 이름 입력 : ")
+        # key_name= input("key 이름 입력 : ")
+        # server_name= input("server 이름 입력 : ") 
+        # num_user=int(input("사용자 수 입력: ")) 
+        with open('fedora-0223.json','r') as f:
             json_data=json.load(f)
-        if(system_num==1):
-            json_data['template']['resources']['mybox']['properties']['image']="ubuntu"
-        elif(system_num==2):
-            json_data['template']['resources']['mybox']['properties']['image']="centos"
-            json_data['template']['resources']['mybox']['properties']['flavor']="ds1G"
-        elif(system_num==3):
-            json_data['template']['resources']['mybox']['properties']['image']="Fedora"
-            del json_data['template']['resources']['myconfig']['properties']['cloud_config']["bootcmd"]
-            print(json_data['template']['resources']['myconfig']['properties']['cloud_config'])
-        json_data['stack_name']=stack_name
-        json_data['template']['resources']['demo_key']['properties']['name']=key_name
-        json_data['template']['resources']['mybox']['properties']['name']=server_name
-        for i in range(num_user):
-            json_data['template']['resources']['myconfig']['properties']['cloud_config']['users'].append("%d"%(i))
-            json_data['template']['resources']['myconfig']['properties']['cloud_config']['chpasswd']["list"].append("%d:%d%d%d%d"%(i,i,i,i,i))
+        # if(system_num==1):
+        #     json_data['template']['resources']['mybox']['properties']['image']="ubuntu"
+        # elif(system_num==2):
+        #     json_data['template']['resources']['mybox']['properties']['image']="centos"
+        #     json_data['template']['resources']['mybox']['properties']['flavor']="ds1G"
+        # elif(system_num==3):
+        #     json_data['template']['resources']['mybox']['properties']['image']="Fedora"
+        #     del json_data['template']['resources']['myconfig']['properties']['cloud_config']["bootcmd"]
+        #     print(json_data['template']['resources']['myconfig']['properties']['cloud_config'])
+        # json_data['stack_name']=stack_name
+        # json_data['template']['resources']['demo_key']['properties']['name']=key_name
+        # json_data['template']['resources']['mybox']['properties']['name']=server_name
+        # for i in range(num_user):
+        #     json_data['template']['resources']['myconfig']['properties']['cloud_config']['users'].append("%d"%(i))
+        #     json_data['template']['resources']['myconfig']['properties']['cloud_config']['chpasswd']["list"].append("%d:%d%d%d%d"%(i,i,i,i,i))
 
             
         user_res = requests.post("http://"+address+"/heat-api/v1/6afe05fbd2cb47a6b149ee3541fb47a6/stacks",

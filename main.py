fa0d2f9253fd389a006b11444d3ebe5ae3f47f8b
drok02 <drok02@naver.com>
2022-07-21 23:48:15 +0900

    다시해볼게

--- main.py
+++ main.py
@@ -6,17 +6,12 @@ from django.views import View
 from django.http import HttpResponse, JsonResponse
 import sys
 import pandas as pd
-import time
-address = "192.168.0.118"
-tenet_id = "91d317f465b84ed2bc9f123eac5f8b07"
+# memberFile=pd.read_csv('test_csv_file.csv')
+#         for i in range(memberFile.shape[0]):
+#             id, name, passwd = memberFile.loc[i]
+address = "192.168.0.54"
 # 토큰 받아오기
 class AccountView():
-    def Request(self,apiURL, jsonData):
-        auth_res = requests.post("http://"+address+apiURL,
-            headers = {'content-type' : 'application/json'},
-            data = json.dumps(jsonData))
-        return auth_res
-
     def token(self):
         # data2 = json.loads(request.body)
         # Admin으로 Token 발급 Body
@@ -40,42 +35,45 @@ class AccountView():
         }  
 
         # Openstack keystone token 발급
-        # auth_res = requests.post("http://"+address+"/identity/v3/auth/tokens",
-        #     headers = {'content-type' : 'application/json'},
-        #     data = json.dumps(token_payload))
-        url="/identity/v3/auth/tokens"
-        auth_res=self.Request(url,token_payload)
+        auth_res = requests.post("http://"+address+"/identity/v3/auth/tokens",
+            headers = {'content-type' : 'application/json'},
+            data = json.dumps(token_payload))
+
         #발급받은 token 출력
         admin_token = auth_res.headers["X-Subject-Token"]
-        print("token : \n",admin_token)
+        print("token",admin_token)
         return admin_token
 
 
     def create_stack(self):
-        start = time.time()
+        #API 요청을 위한 인증 토큰 발급
         admin_token= self.token()
-        system_num=int(input("원하는 시스템 입력: 1.Ubuntu 2.CentOS 3.Fedora\n"))
-        stack_name= int(input("Flavor 선택 : 1. m1.nano 2. m1.small 3. m1.medium 4. m1.large\n"))
-        key_name= int(input("네트워크 입력 : 1. public 2. private 3. shared\n"))
-        server_name=int(input("SW 입력 : 1. apache2 2. pwgen 3. libguestfs-tools 4. pastebinit\n")) 
 
-        if(system_num==1):
-            with open('main.json','r') as f:
-                json_data=json.load(f)
-        elif(system_num==2):
-            with open('centos.json','r') as f:
-                json_data=json.load(f)
-        elif(system_num==3):
-            with open('fedora-0223.json','r') as f:
-                json_data=json.load(f)
-     
-            #address heat-api v1 프로젝트 id stacks
-        user_res = requests.post("http://"+address+"/heat-api/v1/"+tenet_id+"/stacks",
+        #사용자 요구사항중 OS 입력
+        osList=['ubuntu.json','centos.json','fedora.json']
+        system_num=int(input("원하는 OS 번호를 입력: 1.Ubuntu 2.CentOS 3.Fedora \n"))
+
+        #사용자 요구사항중 SW 입력
+        softInfo = ['vim','filezilla','ftp','default-jdk','synaptic']
+        softNum = list(map(int, input("원하는 소프트웨어 번호를 입력: 1.vim 2.filezilla 3.ftp 4.default-jdk 5.synaptic  입력예시: 1 3 4\n").split()))
+        
+        #기본 템플릿 생성
+        with open(osList[system_num-1], 'r') as f:
+            json_data = json.load(f)
+        
+        #기본 템플릿 항목에 요구사항 입력
+        for i in softNum:
+            json_data['template']['resources']['myconfig']['properties']['cloud_config']['packages'].append(softInfo[i - 1])
+        print(json_data['template']['resources']['myconfig']['properties']['cloud_config']['packages'])
+
+        #클라우드 플랫폼 오케스트레이션 요청
+        user_res = requests.post("http://"+address+"/heat-api/v1/6afe05fbd2cb47a6b149ee3541fb47a6/stacks",
             headers = {'X-Auth-Token' : admin_token},
             data = json.dumps(json_data))
+        
+        #맞춤형 가상머신 생성 결과 출력
         print("stack생성 ",user_res)
-        end = time.time()
-        print("종래 시스템의 오케스트레이션 가상환경 생성 시간 : ", end-start)
+
 
 def main():
     f=AccountView()
